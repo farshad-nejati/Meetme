a5868fa5540f207c6006cfe1771592ff08242840
farshad-nejati <nejati.farshad93@gmail.com>
2024-04-08 13:28:27 +0330

    add icons

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const sidebarLinks = [
+  { label: 'Home', route: '/', imgUrl: './icons/Home.svg' },
+  { label: 'Upcoming', route: '/upcoming', imgUrl: './icons/upcoming.svg' },
+  { label: 'Previous', route: '/previous', imgUrl: './icons/previous.svg' },
+  { label: 'Recordings', route: '/recordings', imgUrl: './icons/Video.svg' },
+  {
+    label: 'Personal Room',
+    route: '/personal-room',
+    imgUrl: './icons/add-presonal.svg',
+  },
+]
